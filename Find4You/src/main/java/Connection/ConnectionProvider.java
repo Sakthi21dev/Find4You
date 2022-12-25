dea5808f2ea32c5ae2c4ef264752beda33bf1883
sakthifeb <sakthi21feb@gmail.com>
2022-12-25 07:58:29 +0530

    Update ConnectionProvider.java

--- Find4You/src/main/java/Connection/ConnectionProvider.java
+++ Find4You/src/main/java/Connection/ConnectionProvider.java
@@ -7,7 +7,7 @@ public class ConnectionProvider {
 	public static Connection getcon() {
 		try {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/find4u","root","S@kthi21");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/find4u","root","password");
 		return con;
 		}catch(Exception e) {
 			System.out.println(e);
